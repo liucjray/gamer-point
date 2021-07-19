bd66c5af0a635a98a00e838f6c914b0c57fd1897
willy08wu07 <willy08wu07@users.noreply.github.com>
2021-07-19 10:29:37 +0800

    補充註解：關於資料表欄位預設長度修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // FIXME: 在正式環境上不需要修正預設長度，考慮拿掉
+        // 參考資料：<https://blog.keniver.com/2017/11/laravel-%E5%B8%B8%E8%A6%8B%E5%95%8F%E9%A1%8C-specified-key-long/>
         Schema::defaultStringLength(191);
     }
 }
